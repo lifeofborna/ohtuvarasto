3be4723ef82df2c45f0e9def7628a5689de84b5a
Borna <borna.jamali@gmail.com>
2022-11-08 17:59:50 +0200

    Added ex 3

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -29,14 +29,14 @@ class TestVarasto(unittest.TestCase):
 
         saatu_maara = self.varasto.ota_varastosta(2)
 
-        self.assertAlmostEqual(saatu_maara,0)
+        self.assertAlmostEqual(saatu_maara, 0)
 
     def test_lisays_palauttaa_oikein_jos_maara_pienempi_kuin_0(self):
         self.varasto.lisaa_varastoon(-2)
 
         saatu_maara = self.varasto.ota_varastosta(2)
 
-        self.assertAlmostEqual(saatu_maara,0)
+        self.assertAlmostEqual(saatu_maara, 0)
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
@@ -56,20 +56,20 @@ class TestVarasto(unittest.TestCase):
     def test_tarkista_virheellinen_tilavuus(self):
         varasto = Varasto(-23)
 
-        self.assertAlmostEqual(varasto.tilavuus,0)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
 
-    #    
+    #
     def test_tarkista_alku_saldo_virheellinen(self):
         varasto = Varasto(0, alku_saldo=-50)
 
         self.assertAlmostEqual(varasto.saldo, 0)
-    
+
     def test_ota_varastosta_negatiivinen(self):
-        self.assertAlmostEqual(self.varasto.ota_varastosta(-23),0.0)
-    
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-23), 0.0)
+
     def test_jos_maara_on_suurempi_kuin_paljonko_mahtuu(self):
         s = self.varasto.lisaa_varastoon(20000)
-        self.assertAlmostEqual(self.varasto.saldo,self.varasto.tilavuus)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
 
     def test_oikea_tulostus(self):
         self.assertAlmostEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
